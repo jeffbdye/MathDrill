4741f601823a9d56a8f05d394d30b5f685366573
Jeff Dye <jeffbdye@gmail.com>
2020-11-04 22:34:16 -0500

    create if not exists

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -2,7 +2,7 @@
 
 mathdrill_now=`date +%Y-%m-%d--%H-%M-%S`
 
-mkdir /var/www/mathdrill.app/archive/$mathdrill_now
+mkdir -p /var/www/mathdrill.app/archive/$mathdrill_now
 mv /var/www/mathdrill.app/public/* /var/www/mathdrill.app/archive/$mathdrill_now
 cp /var/lib/jenkins/workspace/MathDrill/public/* /var/www/mathdrill.app/public
 chgrp buildusers /var/www/mathdrill.app/public
