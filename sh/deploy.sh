589c37efc83aed4b82d753c915aa1ef66f3257d4
Jeff Dye <jeffbdye@gmail.com>
2020-11-04 23:02:10 -0500

    also this

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -2,8 +2,12 @@
 
 mathdrill_now=`date +%Y-%m-%d--%H-%M-%S`
 
-mkdir -p /var/www/mathdrill.app/archive/$mathdrill_now
-mv /var/www/mathdrill.app/public/* /var/www/mathdrill.app/archive/$mathdrill_now
+if [ -d '/var/www/mathdrill.app/public' ] 
+then
+  mkdir -p /var/www/mathdrill.app/archive/$mathdrill_now
+  mv /var/www/mathdrill.app/public/* /var/www/mathdrill.app/archive/$mathdrill_now
+fi
+
 cp /var/lib/jenkins/workspace/MathDrill/public/* /var/www/mathdrill.app/public
 chgrp buildusers /var/www/mathdrill.app/public
 unset mathdrill_now
