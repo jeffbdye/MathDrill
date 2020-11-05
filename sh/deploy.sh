2681ac472fae5cf6b172cfaae050c76d67f371af
Jeff Dye <jeffbdye@gmail.com>
2020-11-04 23:04:47 -0500

    better variable use

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 mathdrill_now=`date +%Y-%m-%d--%H-%M-%S`
+public_path='/var/www/mathdrill.app/public'
 
-if [ -d '/var/www/mathdrill.app/public' ] 
+if [ -d $public_path ] 
 then
   mkdir -p /var/www/mathdrill.app/archive/$mathdrill_now
   mv /var/www/mathdrill.app/public/* /var/www/mathdrill.app/archive/$mathdrill_now
+else
+  mkdir -p $public_path
 fi
 
-cp /var/lib/jenkins/workspace/MathDrill/public/* /var/www/mathdrill.app/public
-chgrp buildusers /var/www/mathdrill.app/public
+cp /var/lib/jenkins/workspace/MathDrill/public/* $public_path
+chgrp buildusers $public_path
+
 unset mathdrill_now
+unset public_path
